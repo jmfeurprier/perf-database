7047cc1af3bf4e32d52e1c2c4ab30b0439463dfe
Jean-Michel Feurprier <jmfeurprier@gromweb.com>
2015-08-15 00:23:55 -0400

    added tests

--- test/unit/perf/Db/QuerySortingTest.php
+++ test/unit/perf/Db/QuerySortingTest.php
@@ -55,4 +55,52 @@ class QuerySortingTest extends \PHPUnit_Framework_TestCase
 
         $this->assertSame('foo DESC, bar ASC, baz DESC', $result);
     }
+
+    /**
+     *
+     */
+    public function testGetClauseWithNoneConstructor()
+    {
+        $querySorting = QuerySorting::none();
+
+        $result = $querySorting->getClause();
+
+        $this->assertSame('NULL', $result);
+    }
+
+    /**
+     *
+     */
+    public function testGetClauseWithAscendingConstructor()
+    {
+        $querySorting = QuerySorting::ascending('foo');
+
+        $result = $querySorting->getClause();
+
+        $this->assertSame('foo ASC', $result);
+    }
+
+    /**
+     *
+     */
+    public function testGetClauseWithDescendingConstructor()
+    {
+        $querySorting = QuerySorting::descending('foo');
+
+        $result = $querySorting->getClause();
+
+        $this->assertSame('foo DESC', $result);
+    }
+
+    /**
+     *
+     */
+    public function testGetClauseWithRandomConstructor()
+    {
+        $querySorting = QuerySorting::random();
+
+        $result = $querySorting->getClause();
+
+        $this->assertSame('RAND() ASC', $result);
+    }
 }
